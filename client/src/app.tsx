ef8fa29387fd6902ed637f3644cf7554ec5d25c2
MaximUgarov <maximkaxaxaxa@mail.ru>
2022-08-16 22:24:00 +0300

    some fix

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import Header from "components/layout/header";
@@ -13,15 +13,10 @@ import { authSelector } from "store/selectors/auth";
 
 
 const App: FC = () => {
-  const [isLoading, setLoading] = useState<boolean>(true);
   const isAuth = useAppSelector(authSelector);
   const { isFetching } = useGetUserQuery(null, { skip: !localStorage.getAccessToken() });
 
-  useEffect(() => {
-    !isFetching && setLoading(false);
-  }, [isFetching]);
-
-  if (isLoading) return <LoaderLayout />;
+  if (isFetching) return <LoaderLayout />;
 
   return (
     <BrowserRouter>
